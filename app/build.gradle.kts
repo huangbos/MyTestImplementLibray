8d382cbbc1cb086e8ada4ea922dc6be50bcb3a39
huangbo <877406173@qq.com>
2023-12-15 19:00:40 +0800

    init number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,8 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 //    implementation(project(":app:mylibrary"))
+//    implementation ("com.github.huangbos:MyTestImplementLibray:v2.0")
+
 
 
 }
